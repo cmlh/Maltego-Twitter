e14b4377a97bceccdf97c1f4015d83bff581fc26
Christian Heinrich <christian.heinrich@cmlh.id.au>
2012-04-24 18:25:02 +1000

    Added ./*

---
+++ Local_Transforms/from_affiliation_twitter-to_friends_count.pl
@@ -0,0 +1,143 @@
+#!/usr/bin/env perl
+# The above shebang is for "perlbrew", otherwise use /usr/bin/perl or the file path quoted for "which perl"
+#
+# Please refer to the Plain Old Documentation (POD) at the end of this Perl Script for further information
+
+use strict;
+use JSON;
+use HTTP::Tiny;
+use URI::Escape;
+use Config::Std;
+
+# #CONFIGURATION Remove "#" for Smart::Comments
+# use Smart::Comments;
+
+my $VERSION = "0.0.1"; # May be required to upload script to CPAN i.e. http://www.cpan.org/scripts/submitting.html
+
+# Command line arguments from Maltego
+my $maltego_selected_entity_value = $ARGV[0];
+
+# "###" is for Smart::Comments CPAN Module
+### \$maltego_selected_entity_value is: $maltego_selected_entity_value;
+
+my $maltego_additional_field_values = $ARGV[1];
+
+# "###" is for Smart::Comments CPAN Module
+### \$maltego_additional_field_values is: $maltego_additional_field_values;
+
+my @maltego_additional_field_values =
+  split( '#', $maltego_additional_field_values );
+
+my %maltego_additional_field_values;
+
+foreach (@maltego_additional_field_values) {
+    my ( $key, $value ) = split( /=/, $_, 2 );
+    $maltego_additional_field_values{"$key"} = "$value";
+}
+
+my $friends = $maltego_additional_field_values{"friends"};
+
+# "###" is for Smart::Comments CPAN Module
+### \$friends is: $friends;
+
+print("<MaltegoMessage>\n");
+print("<MaltegoTransformResponseMessage>\n");
+print("\t<UIMessages>\n");
+print(
+"\t\t<UIMessage MessageType=\"Inform\">To Twitter Friends Count v$VERSION</UIMessage>\n"
+);
+print("\t</UIMessages>\n");
+print("\t<Entities>\n");
+
+print("\t\t<Entity Type=\"maltego.Phrase\"><Value>$friends</Value></Entity>\n");
+print("\t</Entities>\n");
+
+# http://ctas.paterva.com/view/Specification#Message_Wrapper
+print("</MaltegoTransformResponseMessage>\n");
+print("</MaltegoMessage>\n");
+
+=head1 NAME
+
+from_friends - "To Number of Twitter Friends"
+
+=head1 VERSION
+
+This documentation refers "To Number of Twitter Friends" Alpha v$VERSION
+
+=head1 CONFIGURATION
+
+Set the value(s) marked as #CONFIGURATION above this POD
+    
+=head1 USAGE
+
+from_affiliation_twitter-to_friends_count.pl $maltego_selected_entity $maltego_additional_field_values
+
+=head1 REQUIRED ARGUEMENTS
+                
+=head1 OPTIONAL ARGUEMENTS
+
+=head1 DESCRIPTION
+
+Returns the Friends Count of a Twitter Account in Malteg
+
+=head1 DEPENDENCIES
+
+=head1 PREREQUISITES
+
+JSON CPAN Module
+Config::Std CPAN Module
+Smart::Comments CPAN Module
+
+=head1 COREQUISITES
+
+=head1 INSTALLATION
+
+=head1 OSNAMES
+
+osx
+
+=head1 SCRIPT CATEGORIES
+
+Web
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+Please refer to the comments beginning with "TODO" in the Perl Code.
+
+=head1 AUTHOR
+
+Christian Heinrich
+
+=head1 CONTACT INFORMATION
+
+http://cmlh.id.au/contact
+
+=head1 MAILING LIST
+
+=head1 REPOSITORY
+
+http://github.com/cmlh/Maltego-Facebook
+
+=head1 FURTHER INFORMATION AND UPDATES
+
+http://cmlh.id.au/tagged/maltego
+
+=head1 LICENSE AND COPYRIGHT
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+
+Copyright 2012 Christian Heinrich
+
+
